e21ffb35e386f926b38c7b757345e397a44cd090
merryejik <s.maria.k@gmail.com>
2013-12-13 12:28:57 +0300

    Completed flip animation

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -25,14 +25,19 @@
 
 -(void)flip:(UITapGestureRecognizer *)tap
 {
-    [UIView transitionWithView:tap.view
+    [self flipAnimateBlock:^(){[self flipCard: tap.view];}
+                   ForView:tap.view];    
+}
+
+-(void)flipAnimateBlock:(void (^)())block ForView:(UIView *)tView
+{
+    [UIView transitionWithView:tView
                       duration:0.5
                        options:UIViewAnimationOptionTransitionFlipFromRight
                     animations:^{
-                        [self flipCard: tap.view];
+                        block();
                     }
                     completion:nil];
-    
 }
 
 
@@ -45,7 +50,13 @@
             PlayingCardView *pView = (PlayingCardView *)cardView;
             PlayingCard *pCard = (PlayingCard *)card;
             
-            pView.faceUp = pCard.isFaceUp;
+            if (pView.faceUp!=pCard.faceUp)
+            {
+                [self flipAnimateBlock:^(){pView.faceUp = pCard.isFaceUp;}
+                               ForView:cardView];
+                
+            }
+            
             pView.playable = !pCard.isUnplayable;
             
             pView.alpha = pCard.isUnplayable ? 0.3 : 1.0;
